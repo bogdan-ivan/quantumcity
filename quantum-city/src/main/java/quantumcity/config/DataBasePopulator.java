8967a23e70bb3fb57406e763a4a2ef6a47cf8121
bogdan-ivan <ibogdanidaniel@gmail.com>
2019-12-14 22:28:50 +0200

    Traffic populator.

--- quantum-city/src/main/java/quantumcity/config/DataBasePopulator.java
+++ quantum-city/src/main/java/quantumcity/config/DataBasePopulator.java
@@ -9,13 +9,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 
 import quantumcity.persistance.entities.Parking;
+import quantumcity.persistance.entities.Traffic;
 import quantumcity.persistance.repos.ParkingRepository;
+import quantumcity.persistance.repos.TrafficRepository;
 
 @Configuration
 public class DataBasePopulator {
 
 	@Autowired
 	ParkingRepository parkingRepository;
+	
+	@Autowired
+	TrafficRepository trafficRepository;
 	
 	@PostConstruct
 	public void generateParkings() {
@@ -35,6 +40,22 @@ public class DataBasePopulator {
 		}
 	}
 		
+	}
+	
+	
+	@PostConstruct
+	public void generateTraffic() {
+		if(trafficRepository.findAll().size() == 0) {
+			List<Traffic> traffics = new ArrayList<Traffic>();
+			traffics.add(new Traffic(100,5,"13 Decembrie",2));
+			traffics.add(new Traffic(10,8,"Bulevard Grivitei",1));
+			traffics.add(new Traffic(20,17,"Cale Bucuresti",1));
+			traffics.add(new Traffic(30,12,"Bulevard Saturn",0));
+			traffics.add(new Traffic(40,16,"Alee Lacramioarelor",0));
+			traffics.add(new Traffic(50,13,"Muresenilor",0));
+			traffics.add(new Traffic(33,14,"Bulevard Garii",2));
+			traffics.add(new Traffic(11,15,"Bulevard Victoriei",0));
+		}
 	}
 	
 }
