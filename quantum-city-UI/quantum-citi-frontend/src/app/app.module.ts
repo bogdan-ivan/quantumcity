b05f8e9c14e5beb418baf9765b96d63a8f132d7a
Hriho <raduhrihoreanu99@yahoo.com>
2019-12-15 01:02:03 +0200

    bag pula

--- quantum-city-UI/quantum-citi-frontend/src/app/app.module.ts
+++ quantum-city-UI/quantum-citi-frontend/src/app/app.module.ts
@@ -3,14 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TrafficComponent } from './components/traffic/traffic.component';
+import { PollutionComponent } from './components/pollution/pollution.component';
+import { ParkingComponent } from './components/parking/parking.component';
+import { WeatherComponent } from './components/weather/weather.component';
+import { HomePageComponent } from './components/home-page/home-page.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TrafficComponent,
+    PollutionComponent,
+    ParkingComponent,
+    WeatherComponent,
+    HomePageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
